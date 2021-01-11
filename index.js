a4ffd05685921273554b6d048cfbf0b2ca7030be
songyouhyun <doong3373@gmail.com>
2021-01-11 22:02:52 +0900

    add cursor and change title color how to change class name

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    const currentColor = title.style.color;
-    if (currentColor === BASE_COLOR){
-        title.style.color = OTHER_COLOR;
-    } else {
-        title.style.color = BASE_COLOR;
-    }
+        title.classList.toggle(CLICKED_CLASS);
+        // toggle = 클라스가 거기 있는지 체크해서 거기 있으면 add 해주고 아니면 remove 해주는 함수
 }
-
 function init() {
-    title.style.color = BASE_COLOR;
     title.addEventListener("click", handleClick);
 }
 init();
-
-function handleOffline(){
-    console.log("It's Offline");
-}
-
-function handleOnline() {
-    console.log("It's Online");
-}
-
-window.addEventListener("offline", handleOffline);
-window.addEventListener("online", handleOnline);
