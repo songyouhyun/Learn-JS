c2780ce82c8c5e8e7bda10885b656645441314cd
songyouhyun <doong3373@gmail.com>
2021-01-11 21:30:32 +0900

    Change title Color to use Conditional

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const title = document.querySelector("#title");
 
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
 function handleClick() {
-    title.style.color = "red";
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
 }
 
-title.addEventListener("click", handleClick);
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
+}
+init();
